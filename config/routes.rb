d38b2afde8733d89fc040be37a7ccae2126a397a
David Haley <djdavidhaley@gmail.com>
2016-07-19 14:31:58 -0600

    Add links to login, signup, and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
