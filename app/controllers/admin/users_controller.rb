b317add0a9c2bcad07b19a158d67404c513d715e
David Haley <djdavidhaley@gmail.com>
2016-07-19 21:32:06 -0600

    Add admin user management

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_filter :authorized?
 
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(10)
     if authorized?
       render :index
     else
